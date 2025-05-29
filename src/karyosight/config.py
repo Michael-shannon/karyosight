51005240422b0253516c59b4f3bde876f924e65c
Michael Shannon <mkjshan@gmail.com>
2025-05-29 16:52:10 -0400

    config

--- src/karyosight/config.py
+++ src/karyosight/config.py
@@ -1,7 +1,7 @@
 # config.py
 from pathlib import Path
 
-MASTER = Path(r"D:\LUNGBUDMASTERTEST_4")  # master directory
+MASTER = Path(r"D:\LUNGBUDLARGETEST___tif")  # master directory
 
 # RAW_DIR         = Path(r"D:\LUNGBUDMASTERTEST_3") / "analyze"
 # STITCHED_DIR    = Path(r"D:\LUNGBUDMASTERTEST_3") / "stitched"
@@ -14,7 +14,7 @@ CROPPED_DIR = MASTER / "cropped"
 
 CONDITION_PREF  = "Condition_"
 MULTIVIEW_CMD   = "multiview-stitcher"      # your CLI tool
-BATCH_GRID_SHAPE= (2, 2)                    # split each full-grid into 2×2 sub-grids
+# BATCH_GRID_SHAPE= (2, 2)                    # split each full-grid into 2×2 sub-grids
 OVERLAP_PERCENT = 10                        # stitcher overlap hint
 OUTPUT_EXT      = ".ome.tiff"               # output format
 
@@ -28,6 +28,19 @@ TILE_SIZE_Y    = 1024              # px, from SizeY
 CHANNELS       = ["CH1","CH2","CH3","CH4","CH5","CH6"]
 REG_CHANNEL    = "CH1"             # registration channel name
 
-MAX_TILES      = 10              
+MAX_TILES      = 36              
+# BATCH_TILE_OVERLAP = 1
+
+# BATCH_GRID_SHAPE = (6, 6)    # how many tiles per side in each sub-grid
+# how many tiles per side in each sub-grid (set to None to auto-calc)
+BATCH_GRID_SHAPE = [12,12]
+
+# if BATCH_GRID_SHAPE is None, we’ll estimate from RAM with:
+SAFETY_FRACTION  = 0.5   # fraction of available RAM to use
+OVERHEAD_FACTOR  = 2.0   # per-tile memory multiplier for Dask/Zarr overhead
 
 
+
+
+
+BATCH_TILE_OVERLAP = 1         # how many tiles extra on each edge for overlap
